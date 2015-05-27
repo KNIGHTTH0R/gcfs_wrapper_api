a0fb6aecaf42dec96ed800e850b4557f3dd17264
Windiana <windiana@giftcard.co.id>
2015-05-27 15:10:10 +0700

    add output to attr reader on order item

--- lib/gcfs_wrapper_api/order_item.rb
+++ lib/gcfs_wrapper_api/order_item.rb
@@ -4,14 +4,14 @@ module Gcfs
       extend Configuration
 
       class OrderItem < Base
-        VALID_ATTRIBUTES =  [:id, :sku, :name, :variants].freeze
+        VALID_ATTRIBUTES =  [:id, :sku, :name, :variants, :output].freeze
         attr_reader *VALID_ATTRIBUTES
 
         def initialize(attributes)
           @id = attributes["id"]
           @sku = attributes["sku"]
           @name = attributes["name"]
-          @output = attributes["output"]
+          @output = attributes["output"] if attributes["output"]
           @variants = attributes["variants"].map{|item| Gcfs::Wrapper::Api::OrderItemVariant.new item } if attributes["variants"]
         end
       end
