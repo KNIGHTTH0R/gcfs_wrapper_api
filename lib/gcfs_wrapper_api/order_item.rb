b5dc8013bd751b7d2a549577b25900fdd8093636
Windiana <windiana@giftcard.co.id>
2015-06-19 14:22:24 +0700

    add readable attributes for datacell order

--- lib/gcfs_wrapper_api/order_item.rb
+++ lib/gcfs_wrapper_api/order_item.rb
@@ -13,6 +13,10 @@ module Gcfs
           @name = attributes["name"]
           @output = attributes["output"] if attributes["output"]
           @variants = attributes["variants"].map{|item| Gcfs::Wrapper::Api::OrderItemVariant.new item } if attributes["variants"]
+          @resultcode = attributes["resultcode"]
+          @message = attributes["message"]
+          @trxid = attributes["trxid"]
+          @ref_trxid = attributes["ref_trxid"]
         end
       end
 
