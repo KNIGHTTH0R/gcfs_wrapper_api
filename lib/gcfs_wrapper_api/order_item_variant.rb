66d08c44642025f7925606ec2a77c93b391b4c39
windy <windiana@windiana-X455LN.(none)>
2015-09-07 17:24:21 +0700

    add program id attribtues to order item variant

--- lib/gcfs_wrapper_api/order_item_variant.rb
+++ lib/gcfs_wrapper_api/order_item_variant.rb
@@ -4,7 +4,7 @@ module Gcfs
       extend Configuration
 
       class OrderItemVariant < Base
-        VALID_ATTRIBUTES =  [:id, :sku, :description, :quantity, :price, :nominal, :subtotal, :design_type, :tada_type].freeze
+        VALID_ATTRIBUTES =  [:id, :sku, :description, :quantity, :price, :nominal, :subtotal, :design_type, :tada_type, :program_id].freeze
         attr_reader *VALID_ATTRIBUTES
 
         def initialize(attributes)
@@ -17,6 +17,7 @@ module Gcfs
           @subtotal = attributes["subtotal"]
           @design_type = attributes["design_type"]
           @tada_type = attributes["tada_type"]
+          @program_id = attributes["program_id"]
         end
       end
 
