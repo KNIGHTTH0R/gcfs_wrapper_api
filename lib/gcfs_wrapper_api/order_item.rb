51b4f6f1862255dc0de8a9d879f5fc19ff9358a8
Windiana <windiana@giftcard.co.id>
2015-11-19 17:39:34 +0700

    add category name to order item

--- lib/gcfs_wrapper_api/order_item.rb
+++ lib/gcfs_wrapper_api/order_item.rb
@@ -4,13 +4,14 @@ module Gcfs
       extend Configuration
 
       class OrderItem < Base
-        VALID_ATTRIBUTES =  [:id, :sku, :name, :variants, :output, :resultcode, :message, :trxid, :ref_trxid].freeze
+        VALID_ATTRIBUTES =  [:id, :sku, :name, :category_name, :variants, :output, :resultcode, :message, :trxid, :ref_trxid].freeze
         attr_reader *VALID_ATTRIBUTES
 
         def initialize(attributes)
           @id = attributes["id"]
           @sku = attributes["sku"]
           @name = attributes["name"]
+          @category_name = attributes["category_name"]
           @output = attributes["output"] if attributes["output"]
           @variants = attributes["variants"].map{|item| Gcfs::Wrapper::Api::OrderItemVariant.new item } if attributes["variants"]
           @resultcode = attributes["resultcode"]
