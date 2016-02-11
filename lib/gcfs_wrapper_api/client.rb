a9f9091096c56f0d812f40c98c02df589abea6fc
Vitorio Santosa <vitorio@giftcard.co.id>
2016-02-11 17:22:43 +0700

    Add Client Code API

--- lib/gcfs_wrapper_api/client.rb
+++ lib/gcfs_wrapper_api/client.rb
@@ -4,13 +4,14 @@ module Gcfs
       extend Configuration
 
       class Client < Base
-        VALID_ATTRIBUTES =  [:name, :address, :billing_address].freeze
+        VALID_ATTRIBUTES =  [:name, :address, :billing_address, :client_code].freeze
         attr_reader *VALID_ATTRIBUTES
 
         def initialize(attributes)
           @name = attributes["name"]
           @address = attributes["address"]
           @billing_address = attributes["billing_address"]
+          @client_code =  attributes["client_code"]
         end
       end
 
