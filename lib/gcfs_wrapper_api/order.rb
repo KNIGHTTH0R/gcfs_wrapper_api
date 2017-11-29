d4b72d24b6c4ecf5b25cd463db8310b9527e04bd
iskandargci <iskandar.zulkarnain@giftcard.co.id>
2017-11-29 18:05:30 +0700

    Adjustment for new Shipping Pricelist Module

--- lib/gcfs_wrapper_api/order.rb
+++ lib/gcfs_wrapper_api/order.rb
@@ -13,7 +13,7 @@ module Gcfs
         ITEMS_ATTRIBUTES = [:items].freeze
         PAYMENTS_ATTRIBUTES = [:payments].freeze
         INPUT_ATTRIBUTES_WITH_ITEMS = INPUT_ATTRIBUTES_WITH_RECEPIENT + ITEMS_ATTRIBUTES
-        TABLE_ATTRIBUTES = [:id, :transaction_id, :invoice_number, :po_number, :order_referral, :client, :histories, :total, :shipping_fee, :total_with_shipping, :payment_type, :payment_status, :dtcell_status, :created_at, :updated_at, :order_type, :request_delivery_date, :tada_type, :design_type, :sent_at].freeze
+        TABLE_ATTRIBUTES = [:id, :transaction_id, :invoice_number, :po_number, :order_referral, :client, :histories, :total, :shipping_fee, :total_with_shipping, :payment_type, :payment_status, :dtcell_status, :created_at, :updated_at, :order_type, :request_delivery_date, :tada_type, :design_type, :sent_at, :shipping_price].freeze
         VALID_ATTRIBUTES =  TABLE_ATTRIBUTES + INPUT_ATTRIBUTES_WITH_ITEMS + DELIVERY_ATTRIBUTES + RECEIVE_ATTRIBUTES + PAYMENTS_ATTRIBUTES
         attr_reader *VALID_ATTRIBUTES
 
@@ -37,6 +37,7 @@ module Gcfs
           @items = attributes["items"].map{|item| Gcfs::Wrapper::Api::OrderDatacellItem.new item } if attributes["items"] and attributes["order_type"] == 'datacell'
           @histories = attributes["histories"].map{|item| Gcfs::Wrapper::Api::OrderHistory.new item } if attributes["histories"]
           @total = attributes["total"]
+          @shipping_price = attributes["shipping_price"]
           @shipping_fee = attributes["shipping_fee"]
           @total_with_shipping = attributes["total_with_shipping"]
           @status = attributes["status"]
